f95e2fb1ac766eff3f4ddd26fbb126d2cb84f3ed
Madihaj14 <madihajk14@gmail.com>
2024-08-14 22:30:21 +0530

    new lang

---
+++ Main.java
@@ -0,0 +1,15 @@
+package JavaPrep;
+
+//que: Take 2 variables, a and b and print their sum
+import java.util.Scanner;
+
+public class Main {
+public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum);
+
+    }
+}
